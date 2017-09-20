9330bebe7b62060ce1353f78a71b9ce16b3e1794
Michal Šaman <m.saman@designplus.cz>
2017-09-20 10:49:00 +0200

    Default template Xamarin.Forms project

---
+++ BckgDetectionTest/BckgDetectionTestPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Xamarin.Forms;
+
+namespace BckgDetectionTest
+{
+    public partial class BckgDetectionTestPage : ContentPage
+    {
+        public BckgDetectionTestPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
